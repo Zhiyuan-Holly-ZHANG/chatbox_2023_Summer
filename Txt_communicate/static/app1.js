a84008b69c15c37f38fad3151b1f2d6fa39a1f46
to-dare <90386822+to-dare@users.noreply.github.com>
2023-06-02 15:21:33 +0800

    2023

--- Txt_communicate/static/app1.js
+++ Txt_communicate/static/app1.js
@@ -1,20 +1,9 @@
-// const chatButton = document.querySelector('.chatbox__button');
-// const chatContent = document.querySelector('.chatbox__support');
-// const icons = {
-//     isClicked: '<img src="./images/icons/chatbox-icon.svg" />',
-//     isNotClicked: '<img src="./images/icons/chatbox-icon.svg" />'
-// }
-// const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
-// chatbox.display();
-// chatbox.toggleIcon(false, chatButton);
-
-
 class Chatbox {
     constructor() {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button'),
+            sendButton: document.querySelector('.send__button')
         }
 
         this.state = false;
@@ -22,11 +11,13 @@ class Chatbox {
     }
 
     display() {
-        const { openButton, chatbox, sendButton } = this.args;
+        const {openButton, chatbox, sendButton} = this.args;
         openButton.addEventListener('click', () => this.toggleState(chatbox))
+
         sendButton.addEventListener('click', () => this.onSendButton(chatbox))
+
         const node = chatbox.querySelector('input');
-        node.addEventListener('keyup', ({ key }) => {
+        node.addEventListener("keyup", ({key}) => {
             if (key == "Enter") {
                 this.onSendButton(chatbox)
             }
@@ -38,7 +29,6 @@ class Chatbox {
 
         if (this.state) {
             chatBox.classList.add('chatbox--active')
-
         } else {
             chatBox.classList.remove('chatbox--active')
         }
@@ -51,41 +41,41 @@ class Chatbox {
         if (txt1 = "") {
             return;
         }
-        let msg1 = { name: "User", message: txt1 }
+
+        let msg1 = {name: "User", message: txt1}
         this.messages.push(msg1);
+
+
         fetch($SCRIPT_POST + '/predict', {
             method: 'POST',
-            body: JSON.stringify({ message: txt1 }),
+            body: JSON.stringify({message: txt1}),
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
             },
-
-        })
+            })
             .then(r => r.json())
             .then(r => {
-                let msg2 = { name: "Sam", message: r.answer };
+                let msg2 = {name: "Sam", message: r.answer};
                 this.messages.push(msg2);
                 this.updateChatText(chatBox)
                 textField.value = ''
             }).catch((error) => {
-                console.error('Error:', error)
-                this.updateChatText(chatBox)
-                textField.value = ''
-            });
+            console.error('Error:', error)
+            this.updateChatText(chatBox)
+            textField.value = ''
+        });
     }
 
     updateChatText(chatBox) {
         var html = '';
         this.messages.slice().reverse().forEach(function (item, index) {
             if (item.name == "Sam") {
-                html += '<div class="messages__item messages__item--visitor"'+item.message
+                html += '<div class="messages__item messages__item--visitor"' + item.message
             } else {
-                html += '<div class="messages__item messages__item--operator"'+item.message
+                html += '<div class="messages__item messages__item--operator"' + item.message
 
             }
         })
     }
 }
-
-
